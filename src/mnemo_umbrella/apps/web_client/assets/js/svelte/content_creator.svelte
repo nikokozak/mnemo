017c38d0b0a975442aba59df3e0540e30bb50342
Nikolai Kozak <nikokozak@gmail.com>
2022-10-01 21:20:39 -0300

    add reactivity to svelte element, basic page is bare

---
+++ src/mnemo_umbrella/apps/web_client/assets/js/svelte/content_creator.svelte
@@ -0,0 +1,19 @@
+<script>
+    let email = null;
+
+    let userList = [];
+
+    function submitEmail() { 
+        console.log("submitting " + email);
+        userList = [...userList, email];
+    }
+</script> 
+
+<input type="text" bind:value={email} placeholder="Your email here" />
+<button class="bg-sky-500 p-2 rounded-lg text-white" on:click={submitEmail}>Submit Email</button>
+
+<ul>
+{#each userList as user}
+    <li>{user}</li> 
+{/each}
+</ul>
--- src/mnemo_umbrella/apps/web_client/assets/js/svelte/test.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    export let name;
-</script>
-
-<h1 class="text-lg">Hey {name}, Phoenix Svelte setup is working correctly!</h1>
