e2bfe4bd48478c3b2c3f4cf95ca9f81d8e4c2227
Nikolai Kozak <nikokozak@gmail.com>
2022-11-06 22:30:12 -0300

    Componentize

--- src/mnemo/lib/mnemo_web/live/components/modal.ex
+++ src/mnemo/lib/mnemo_web/live/components/modal.ex
@@ -3,10 +3,10 @@ defmodule MnemoWeb.Live.Components.Modal do
 
   attr :button_text, :string, required: true
   attr :button_class, :string, default: nil
-  slot :icon
-  slot :button_left
-  slot :button_right
-  slot :inner_block
+  slot(:icon)
+  slot(:button_left)
+  slot(:button_right)
+  slot(:inner_block)
 
   def confirmation(assigns) do
     ~H"""
